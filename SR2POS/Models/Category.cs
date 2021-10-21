1424286bf83c0c9a496df112e05f63ae6ad1eb16
samoeun chhay <samoeunchh@gmail.com>
2021-10-21 10:02:00 +0700

    Initial

---
+++ SR2POS/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR2POS.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid CategoryId { get; set; }
+        [MaxLength(50)]
+        [Required]
+        [Display(Name ="Category Name")]
+        public string CategoryName { get; set; }
+    }
+}
