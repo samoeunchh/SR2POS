0ce435cd913b7b7960d1c10019dce8f70e71227c
samoeun chhay <samoeunchh@gmail.com>
2021-11-04 11:00:05 +0700

    Sale Product

---
+++ SR2POS/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR2POS.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Guid CustomerId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="Customer Name")]
+        public string CustomerName { get; set; }
+        [Phone]
+        [MaxLength(15)]
+        public string Mobile { get; set; }
+        [DataType(DataType.MultilineText)]
+        [MaxLength(100)]
+        public string Address { get; set; }
+        [EmailAddress]
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
